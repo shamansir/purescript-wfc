bcd4b732c3a34b5c3deb0ce67a262fc6b9800a5c
shaman.sir <shaman.sir@gmail.com>
2026-07-06 15:13:31 +0200

    add more samples to the Demo page

--- test/Demo/src/Demo/Samples.purs
+++ test/Demo/src/Demo/Samples.purs
@@ -98,5 +98,106 @@ circuit =
   , periodic: false
   }
 
+-- Patterns below inspired by the overlapping-model samples in
+-- https://github.com/mxgmn/WaveFunctionCollapse (Maze, Cave, Skyline, Village),
+-- hand-authored as small int grids since this repo has no bitmap loader.
+
+maze :: SampleDef
+maze =
+  { name: "Maze"
+  , grid:
+      [ [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
+      , [1, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1]
+      , [1, 0, 1, 0, 1, 0, 1, 0, 1, 1, 1]
+      , [1, 0, 1, 0, 1, 0, 0, 0, 1, 0, 1]
+      , [1, 0, 1, 1, 1, 1, 1, 0, 1, 0, 1]
+      , [1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1]
+      , [1, 1, 1, 1, 1, 0, 1, 1, 1, 0, 1]
+      , [1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 1]
+      , [1, 0, 1, 0, 1, 1, 1, 0, 1, 0, 1]
+      , [1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1]
+      , [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
+      ]
+  , palette: \v -> if v == 0 then "#f8f4e8" else "#111111"
+  , outW: 22
+  , outH: 22
+  , n: 3
+  , periodic: false
+  }
+
+cave :: SampleDef
+cave =
+  { name: "Cave"
+  , grid:
+      [ [1, 1, 1, 1, 0, 0, 1, 1, 1, 1]
+      , [1, 1, 0, 0, 0, 0, 0, 0, 1, 1]
+      , [1, 0, 0, 1, 1, 1, 0, 0, 0, 1]
+      , [0, 0, 1, 1, 1, 1, 1, 0, 0, 0]
+      , [0, 1, 1, 1, 0, 1, 1, 1, 0, 0]
+      , [0, 1, 1, 0, 0, 0, 1, 1, 0, 0]
+      , [0, 0, 1, 1, 0, 1, 1, 0, 0, 0]
+      , [1, 0, 0, 1, 1, 1, 0, 0, 1, 1]
+      , [1, 1, 0, 0, 0, 0, 0, 1, 1, 1]
+      , [1, 1, 1, 0, 0, 1, 1, 1, 1, 1]
+      ]
+  , palette: \v -> if v == 0 then "#c9b28a" else "#2b1d0e"
+  , outW: 20
+  , outH: 20
+  , n: 3
+  , periodic: true
+  }
+
+skyline :: SampleDef
+skyline =
+  { name: "Skyline"
+  , grid:
+      [ [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+      , [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+      , [0, 0, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0]
+      , [0, 0, 0, 0, 0, 0, 2, 1, 0, 0, 0, 0, 2, 0, 0, 0]
+      , [0, 0, 0, 1, 0, 0, 1, 1, 2, 0, 0, 0, 1, 0, 0, 0]
+      , [0, 0, 0, 1, 2, 0, 1, 2, 1, 0, 0, 1, 1, 0, 0, 1]
+      , [0, 1, 0, 2, 1, 0, 2, 1, 1, 0, 1, 1, 2, 0, 0, 2]
+      , [0, 1, 2, 1, 1, 0, 1, 1, 2, 1, 1, 2, 1, 0, 2, 1]
+      , [1, 2, 1, 1, 2, 1, 1, 2, 1, 1, 2, 1, 1, 2, 1, 1]
+      , [2, 1, 1, 2, 1, 1, 2, 1, 1, 2, 1, 1, 2, 1, 1, 2]
+      ]
+  , palette: \v ->
+      if v == 0 then "#0b1e3a"
+      else if v == 1 then "#1c1c28"
+      else "#ffd76a"
+  , outW: 32
+  , outH: 20
+  , n: 3
+  , periodic: true
+  }
+
+village :: SampleDef
+village =
+  { name: "Village"
+  , grid:
+      [ [1, 1, 1, 1, 1, 0, 0, 1, 1, 1, 1, 1]
+      , [1, 2, 2, 1, 2, 0, 0, 1, 2, 2, 1, 1]
+      , [1, 2, 2, 1, 2, 0, 0, 1, 2, 2, 1, 1]
+      , [1, 1, 1, 1, 1, 0, 0, 1, 1, 1, 1, 1]
+      , [1, 1, 1, 1, 1, 0, 0, 1, 1, 1, 1, 1]
+      , [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+      , [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+      , [1, 1, 1, 1, 1, 0, 0, 1, 1, 1, 1, 1]
+      , [1, 2, 2, 1, 2, 0, 0, 1, 2, 2, 1, 1]
+      , [1, 2, 2, 1, 2, 0, 0, 1, 2, 2, 1, 1]
+      , [1, 1, 1, 1, 1, 0, 0, 1, 1, 1, 1, 1]
+      , [1, 1, 1, 1, 1, 0, 0, 1, 1, 1, 1, 1]
+      ]
+  , palette: \v ->
+      if v == 0 then "#5c5248"
+      else if v == 1 then "#4a7c3a"
+      else "#8a5a2b"
+  , outW: 24
+  , outH: 24
+  , n: 3
+  , periodic: true
+  }
+
 samples :: Array SampleDef
-samples = [ checkerboard, rooms, knot, circuit ]
+samples = [ checkerboard, rooms, knot, circuit, maze, cave, skyline, village ]
